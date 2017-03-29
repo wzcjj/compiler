2a92e5e49b7a962f298e97f0b8f309391e505f01
wzcjj <438459483@qq.com>
2017-03-30 00:56:52 +0800

    modified main.c

--- src/main.c
+++ src/main.c
@@ -3,12 +3,12 @@ extern FILE* yyin;
 int main(int argc, char** argv) {
     if (argc <= 2) return 1;
     FILE *fin = fopen(argv[1], "r");
-    FILE *fout = fopen(argv[2], "w");
+    FILE *fout = freopen(argv[2], "w", stdout);
     if (!fin) {
         perror(argv[1]);
         return 1;
     }
-    yyrestart(f);
+    yyrestart(fin);
     yyparse();
     return 0;
 }
