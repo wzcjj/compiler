16db2e8277feaf6ad0c239beb3afbd6176b2145c
wzcjj <438459483@qq.com>
2017-04-22 20:06:28 +0800

    add list.c

---
+++ src/list.c
@@ -0,0 +1,27 @@
+#include "list.h"
+
+void listAdd(List *prev, List *next, List *node) {
+    node->prev = prev;
+    node->next = next;
+    if (prev != NULL) prev->next = node;
+    if (next != NULL) next->prev = node;
+}
+
+void listAddBefore(List *list, List *node) {
+    listAdd(list->prev, list, node);
+}
+
+void listAddAfter(List *list, List *node) {
+    listAdd(list, list->next, node);
+}
+
+void listDelete(List *node) {
+    List *prev = node->prev, *next = node->next;
+    if (prev != NULL) prev->next = next;
+    if (next != NULL) next->prve = prev;
+}
+
+void listInit(List *list) {
+    list->prev = list->next = list;
+}
+
