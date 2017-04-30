636936dad7461245e069101e356baaa391b4cd0d
wzcjj <438459483@qq.com>
2017-04-30 21:37:25 +0800

    >  run 141250011 wzcjj Darwin wzcjjdeMacBook-Pro.local 16.3.0 Darwin Kernel Version 16.3.0: Thu Nov 17 20:23:58 PST 2016; root:xnu-3789.31.2~1/RELEASE_X86_64 x86_64 21:37  up 1 day,  1:46, 2 users, load averages: 1.50 1.49 1.48 2fdc48fcc7853c4238d988d8d53a12d7b8303af3

--- src/analyse.c
+++ src/analyse.c
@@ -86,12 +86,12 @@ static void analyseExtDefList(TreeNode *p) {
     if (p == NULL) return;
     getChilds(p);
     analyseExtDef(childs[1]);
-    return;
     analyseExtDefList(childs[2]);
 }
 
 static void analyseExtDef(TreeNode *p) {
     getChilds(p);
+    return;
     Type *type = analyseSpecifier(childs[1]);
     if (isSyntax(childs[2], ExtDecList)) {
         analyseExtDecList(childs[2], type);
