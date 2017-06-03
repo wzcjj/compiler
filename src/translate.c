5bec77d4b6dcd11cee64548cc3e0590708212885
wzcjj <438459483@qq.com>
2017-06-03 14:22:04 +0800

    add translate functions.

--- src/translate.c
+++ src/translate.c
@@ -76,9 +76,9 @@ static InterCodes *translateDef(TreeNode*);
 static InterCodes *translateDecList(TreeNode*);
 static InterCodes *translateDec(TreeNode*);
 static InterCodes *translateStmtList(TreeNode*);
+static InterCodes *translateStmt(TreeNode*);
 static InterCodes *translateCond(TreeNode*, Operand*, Operand*);
 static InterCodes *translateArgs(TreeNode*, List*, Args*);
-static InterCodes *translateStmt(TreeNode*);
 
 InterCodes *translateCompSt(TreeNode *p, Func *func) {
     getChilds(p);
@@ -147,6 +147,108 @@ static InterCodes *translateDec(TreeNode *p) {
         interCodeInsert(irs, newInterCode2(ASSIGN, var, op));
     }
     return irs;
+}
+
+static InterCodes *translateStmtList(TreeNode *p) {
+    Assert(isSyntax(p,StmtList));
+    getChilds(p);
+    InterCodes *irs = translateStmt(childs[1]);
+    if (childscnt == 2) interCodesBind(irs, translateStmtList(childs[2]));
+    return irs;
+}
+
+static InterCodes *translateStmt(TreeNode *p) {
+    Assert(isSyntax(p, Stmt));
+    getChilds(p);
+    if (isSyntax(childs[1], Exp)) {
+        return translateExp(childs[1], NULL);
+    }
+    else if (isSyntax(childs[1], CopmSt)) {
+        return interCodeStackGet();
+    }
+    else if (isSyntax(childs[1], RETURN)) {
+        Operand *op = newTempOperand();
+        InterCodes *irs = translateExp(childs[2], op);
+        InterCode *ir = newInterCode1(RETURN, op);
+        interCodeInsert(irs, ir);
+        return irs;
+    }
+    else if (isSyntax(childs[1], IF)) {
+        bool el = childscnt == 7;
+        TreeNode *stmt1, *stmt2;
+        stmt1 = childs[5];
+        if (el) stmt2 = childs[7];
+        else stmt2 = NULL;
+        Operand *label1 = newLabelOperand();
+        Operand *label2 = newLabelOperand();
+        Operand *label3 = NULL;
+        InterCodes *irs = translateCond(childs[3], label1, label2);
+        interCodeInsert(irs, newInterCode1(DEF_LABEL, label1));
+        interCodesBind(irs, translateStmt(stmt1));
+        if (stmt2) {
+            label3 = newLabelOperand();
+            interCodeInsert(irs, newInterCode1(GOTO, label3));
+        }
+        interCodeInsert(irs, newInterCode1(DEF_LABEL, label2));
+        if (stmt2) {
+            interCodesBind(irs, translateStmt(stmt2));
+            interCodeInsert(irs, newInterCode1(DEF_LABEL, label3));
+        }
+        return irs;
+    }
+    else {
+        Operand *label1 = newLabelOperand();
+        Operand *label2 = newLabelOperand();
+        Operand *label3 = newLabelOperand();
+        InterCodes *irs = newInterCodes();
+        interCodeInsert(irs, newInterCode1(DEF_LABEL, label1));
+        interCodesBind(irs, translateCond(childs[3], label2, label3));
+        interCodeInsert(irs, newInterCode1(DEF_LABEL, label2));
+        interCodesBind(irs, translateStmt(childs[5]));
+        interCodeInsert(irs, newInterCode1(GOTO, label1));
+        return interCodeInsert(irs, newInterCode1(DEF_LABEL, label3));
+    }
+}
+
+static InterCodes *translateCond(TreeNode *p, Operand *labeltrue,
+                                 Operand *labelfalse) {
+    Assert(isSyntax(p, Exp));
+    getChilds(p);
+    if (isSyntax(childs[1], NOT)) {
+        return translateCond(childs[2], labelfalse, labeltrue);
+    }
+    else if (isSyntax(childs[2], RELOP)) {
+        Operand *op1 = newTempOperand();
+        Operand *op2 = newTempOperand();
+        InterCodes *irs = translateExp(childs[1], op1);
+        InterCodes *irs2 = translateExp(childs[3], op2);
+        interCodesBind(irs, irs2);
+        InterCode *ir = newInterCode3(GOTO_WITH_COND, labeltrue, op1, op2);
+        ir->relop = childs[2]->text;
+        interCodeInsert(irs, ir);
+        return interCodeInsert(irs, newInterCode1(GOTO, labelfalse));
+    }
+    else if (isSyntax(childs[2], AND)) {
+        Operand *label = newLabelOperand();
+        InterCodes *irs = translateCond(childs[1], label, labelfalse);
+        interCodeInsert(irs, newInterCode1(DEF_LABEL, label));
+        return interCodesBind(irs, translateCond(childs[3], labeltrue, labelfalse));
+    }
+    else if (isSyntax(childs[2], OR)) {
+        Operand *label = newLabelOperand();
+        InterCodes *irs = translateCond(childs[1], labeltrue, label);
+        interCodeInsert(irs, newInterCode1(DEF_LABEL, label));
+        return interCodesBind(irs, translateCond(childs[3], labeltrue, labelfalse));
+    }
+    else {
+        Operand *op = newTempOperand();
+        InterCodes *irs = translateExp(p, op);
+        InterCode *ir = newInterCode3(GOTO_WITH_COND, labeltrue, op, CONST_ZERO);
+        ir->relop = "!=";
+        interCodeInsert(irs, ir);
+        ir = newInterCode1(GOTO, labelfalse);
+        return interCodeInsert(irs, ir);
+    }
 }
 
 static InterCodes *translateArgs(TreeNode *p, List *curarg, Args *args) {
